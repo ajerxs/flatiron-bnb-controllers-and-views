2d5b5ae07b301f560ed2be3b047ffe2f87cb53a7
Andrew Eames <ajerxs@gmail.com>
2021-09-03 12:30:42 -0400

    commit tree 7bdbe241e2738343929b8357b5e98b1924f58620

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,13 @@
 class ListingsController < ApplicationController
+
+    def index
+        if params[:start_date] && params[:end_date]
+            @listings = Listing.
+        @listings = Listing.all 
+    end
+
+    def show
+        @listing = Listing.find(params[:id])
+    end
+
 end
